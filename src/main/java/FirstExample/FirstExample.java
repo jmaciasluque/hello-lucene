e7a8e91431ecf62474d430c4e6fe2393fc42d410
Mac Luq <macluq.lists@gmail.com>
2012-11-15 11:01:20 +0000

    Initial commit in the local repository

---
+++ src/main/java/FirstExample/FirstExample.java
@@ -0,0 +1,84 @@
+package FirstExample;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
+public class FirstExample {
+
+	public static void main(String[] args) throws IOException, ParseException {
+		// 0. Specify the analyzer for tokenizing text.
+		//    The same analyzer should be used for indexing and searching
+		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
+
+		// 1. create the index
+		Directory index = new RAMDirectory();
+
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
+
+		IndexWriter w = new IndexWriter(index, config);
+		addDoc(w, "Lucene in Action", "193398817");
+		addDoc(w, "Lucene for Dummies", "55320055Z");
+		addDoc(w, "Managing Gigabytes", "55063554A");
+		addDoc(w, "The Art of Computer Science", "9900333X");
+		w.close();
+
+		// 2. query
+		String querystr = args.length > 0 ? args[0] : "lucene";
+
+		// the "title" arg specifies the default field to use
+		// when no field is explicitly specified in the query.
+		Query q = null;
+		try {
+			q = new QueryParser(Version.LUCENE_40, "title", analyzer).parse(querystr);
+		} catch (org.apache.lucene.queryparser.classic.ParseException e) {
+			e.printStackTrace();
+		}
+
+		// 3. search
+		int hitsPerPage = 10;
+		IndexReader reader = DirectoryReader.open(index);
+		IndexSearcher searcher = new IndexSearcher(reader);
+		TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
+		searcher.search(q, collector);
+		ScoreDoc[] hits = collector.topDocs().scoreDocs;
+
+		// 4. display results
+		System.out.println("Found " + hits.length + " hits.");
+		for (int i = 0; i < hits.length; ++i) {
+			int docId = hits[i].doc;
+			Document d = searcher.doc(docId);
+			System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+		}
+
+		// reader can only be closed when there
+		// is no need to access the documents any more.
+		reader.close();
+	}
+
+	private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+		Document doc = new Document();
+		doc.add(new TextField("title", title, Field.Store.YES));
+
+		// use a string field for isbn because we don't want it tokenized
+		doc.add(new StringField("isbn", isbn, Field.Store.YES));
+		w.addDocument(doc);
+	}
+}
